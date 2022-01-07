c3c5acaba1679b51b225647b73917abc87fb4139
Doru-Florin Blanzeanu <blanzeanu.doru@protonmail.com>
2022-01-07 21:27:37 +0200

    Add OrderBook with some tests
    Signed-off-by: Doru-Florin Blanzeanu <blanzeanu.doru@protonmail.com>

--- src/orderbook/mod.rs
+++ src/orderbook/mod.rs
@@ -5,6 +5,13 @@ pub enum Side {
     Sell,
 }
 
+pub enum Response {
+    Acknowledge,
+    Best,
+    Reject,
+    Trade,
+}
+
 #[derive(Debug)]
 struct Order {
     user_id: i64,
@@ -28,3 +35,114 @@ impl Order {
     }
 
 }
+
+
+#[derive(Debug)]
+pub struct OrderBook {
+    ticker: String,
+    /// Ordered vector of asks
+    asks: Vec<Order>,
+    /// Ordered vector of bids
+    bids: Vec<Order>,
+}
+
+impl OrderBook {
+    pub fn new(ticker: &str) -> Self {
+        OrderBook {
+            ticker: String::from(ticker),
+            asks: vec![],
+            bids: vec![],
+        }
+    }
+
+    pub fn ticker(&self) -> &str {
+        self.ticker.as_str()
+    }
+
+    // maybe use binary search for performance here
+    fn sorted_position(side: Side, v: &Vec<Order>, price: i64) -> usize {
+        let mut pos = 0;
+        for (k, o) in v.iter().enumerate() {
+            match &side {
+                Side::Buy => {
+                    if price > o.price() {
+                        pos = k;
+                    }
+                }
+                Side::Sell => {
+                    if price < o.price() {
+                        pos = k;
+                    }
+                }
+            }
+        }
+
+        pos
+    }
+
+    pub fn new_order(&mut self, side: Side, price: i64, qty: i64, user_id: i64, order_id: i64) -> Response {
+        let mut res = Response::Reject;
+        let order = Order::new(price, qty, user_id, order_id);
+
+        match side {
+            Side::Buy => {
+                let pos = OrderBook::sorted_position(Side::Buy, &self.bids, price);
+
+                // Check if this crosses the book
+                if self.asks.len() > 0 {
+                    if self.asks[0].price() <= price {
+                        res = Response::Reject;
+                    }
+                }
+
+                self.bids.insert(pos, order);
+                res = Response::Acknowledge;
+            }
+            Side::Sell => {
+                let pos = OrderBook::sorted_position(Side::Sell, &self.bids, price);
+                self.asks.insert(pos, order)
+            }
+        }
+
+        res
+    }
+
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_empty_orderbook() {
+        let ob = OrderBook::new("TSLA");
+        assert_eq!("TSLA", ob.ticker());
+    }
+
+    #[test]
+    fn test_add_1_bid() {
+        let mut ob = OrderBook::new("TSLA");
+        ob.new_order(Side::Buy, 10, 100, 1, 1);
+
+        assert_eq!("TSLA", ob.ticker());
+    }
+
+    #[test]
+    fn test_add_1_bid_1_ask() {
+        let mut ob = OrderBook::new("TSLA");
+        ob.new_order(Side::Buy, 10, 100, 1, 1);
+        ob.new_order(Side::Sell, 10, 100, 1, 1);
+
+        assert_eq!("TSLA", ob.ticker());
+    }
+
+    #[test]
+    fn test_add_3_bids_verfy_sorted() {
+        let mut ob = OrderBook::new("TSLA");
+        ob.new_order(Side::Buy, 10, 100, 1, 1);
+        ob.new_order(Side::Buy, 12, 100, 1, 1);
+        ob.new_order(Side::Buy, 11, 100, 1, 1);
+        println!("{:?}", ob);
+        assert_eq!("TSLA", ob.ticker());
+    }
+}
+
